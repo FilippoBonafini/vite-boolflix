a221ebf6e98d0920c406f0d4602b5c873443671b
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-30 01:07:16 +0200

    .

--- src/components/AppResultsCards.vue
+++ src/components/AppResultsCards.vue
@@ -38,7 +38,7 @@ export default {
         orderSeriesList() {
             return this.store.tvSeriesList.sort((a, b) => {
                 if (a.popularity > b.popularity) return -1;
-                if (a.popularity > b.popularity) return 1;
+                if (a.popularity < b.popularity) return 1;
                 return 0;
             });
         }
