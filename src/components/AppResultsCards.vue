cd9fc7889dc76e5d9f7c9902108ab87d4fe8a5b1
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-30 01:06:56 +0200

    Ordinata lista per popolarità

--- src/components/AppResultsCards.vue
+++ src/components/AppResultsCards.vue
@@ -18,6 +18,30 @@ export default {
         return {
             store
         }
+    },
+    methods: {
+        // DIVIDO PER 2 UN NUMERO E LO ARROTONDO PER ECCESSO A INTERO 
+        valutation(number) {
+            return Number((number / 2).toFixed())
+        }
+    },
+    computed: {
+        // MOSTRO I FILM IN ORDINE DI POPOLARITA'
+        orderFilmList() {
+            return this.store.filmList.sort((a, b) => {
+                if (a.popularity > b.popularity) return -1;
+                if (a.popularity < b.popularity) return 1;
+                return 0;
+            });
+        },
+        // MOSTRO LE SERIE IN ORDINE DI POPOLARITA'
+        orderSeriesList() {
+            return this.store.tvSeriesList.sort((a, b) => {
+                if (a.popularity > b.popularity) return -1;
+                if (a.popularity > b.popularity) return 1;
+                return 0;
+            });
+        }
     }
 }
 </script>
@@ -28,19 +52,19 @@ export default {
         <h1>results films:</h1>
         <ul>
             <!-- CREO TANTI LIST ITEM QUANTI SONO GLI ELEMENTI NELL'ARRAY DEI FILM  -->
-            <li v-for="film in store.filmList">
+            <li v-for="film in orderFilmList">
                 <!-- PASSO LE PROPS AD OGNI CARD  -->
                 <AppCard :title="film.title" :originalTitle="film.original_title" :lenguage="film.original_language"
-                    :valutation="film.vote_average" :imagePath="film.backdrop_path" />
+                    :valutation="valutation(film.vote_average)" :imagePath="film.backdrop_path" />
             </li>
         </ul>
         <h1>results series:</h1>
         <ul>
             <!-- CREO TANTI LIST ITEM QUANTI SONO GLI ELEMENTI NELL'ARRAY DELLE SERIE  -->
-            <li v-for="series in store.tvSeriesList">
+            <li v-for="series in orderSeriesList">
                 <!-- PASSO LE PROPS AD OGNI CARD  -->
                 <AppCard :title="series.name" :originalTitle="series.original_name" :lenguage="series.original_language"
-                    :valutation="series.vote_average" :imagePath="series.backdrop_path" />
+                    :valutation="valutation(series.vote_average)" :imagePath="series.backdrop_path" />
             </li>
         </ul>
     </div>
